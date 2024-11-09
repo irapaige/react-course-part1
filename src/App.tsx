2d419725188df7350dda01402d10da9a5b583a16
irapaige <irapaige@hotmail.com>
2024-11-08 20:42:13 -0500

    Listgroup Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import UserList from "./components/UserList";
+import ListGroup from "./components/ListGroup/ListGroup";
+
 
 function App() {
-  return <UserList />;
+  let items=["New York", "Atlanta", "Mexico City"]
+
+  return(
+    <div>
+   <ListGroup items={items} heading="Cities" onSelectItem={function (item: string): void {
+        throw new Error("Function not implemented.");
+      } } />;
+   </div>
+  )
 }
 
 export default App;
